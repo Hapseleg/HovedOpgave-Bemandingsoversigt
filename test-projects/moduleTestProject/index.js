d928878dcc43f299be705a9a5c574059fe357e63
Hapseleg <hapse@hh4.dk>
2018-10-21 20:14:08 +0200

    module test project

---
+++ test-projects/moduleTestProject/index.js
@@ -0,0 +1,5 @@
+var test = require('./modules/test')
+
+console.log(test.publicfunc());
+
+console.log(test.abc);
---
+++ test-projects/moduleTestProject/modules/test.js
@@ -0,0 +1,7 @@
+//https://www.w3schools.com/js/js_es6.asp
+
+var abc = 1;
+
+exports.publicfunc = function(){
+    return abc;
+}
